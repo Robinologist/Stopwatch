d2258c44dc348f50b6b63c7f9388786707baf3e9
Upgradr <Robinologist@gmail.com>
2023-05-11 10:55:23 -0400

    changed method of displaying time to use appends & fixed an issue with ticks reaching and displaying 100

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -12,44 +12,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+//-----------------------------------------------------------------------------------------------------------
         
+        //setup TimerUpdate function to trigger every 1/100th of a sec
         let timer = Timer.scheduledTimer(timeInterval: 0.01,target: self,selector: #selector(TimerUpdate),userInfo: nil,repeats: true)
     }
+    
+//-----------------------------------------------------------------------------------------------------------
+    
+    //TimerUpdate function (called every 1/100th of a second)
     @objc private func TimerUpdate() {
+        
+        //if the button is in the on state (stored by buttonstate variable)
         if buttonstate == 1 {
             
             //add to tick score every 0.01 seconds
             ticks += 1
             
-            
-            //update label (if ticks < 10 -- add a 0 before the digit to keep the spacing consistant)
-            if ticks >= 10 {
-                if minutes == 0 {
-                    LabelOutlet.text = String(seconds)+":"+String(ticks)
-                }
-                else if hours == 0 {
-                    LabelOutlet.text = String(minutes)+":"+String(seconds)+":"+String(ticks)
-                }
-                else {
-                    LabelOutlet.text = String(hours)+":"+String(minutes)+":"+String(seconds)+":"+String(ticks)
-                }
-            }
-            
-            //update label (if ticks >= 10)
-            if ticks < 10 {
-                
-                if minutes == 0 {
-                    LabelOutlet.text = String(seconds)+":0"+String(ticks)
-                }
-                else if hours == 0 {
-                    LabelOutlet.text = String(minutes)+":"+String(seconds)+":0"+String(ticks)
-                }
-                else {
-                    LabelOutlet.text = String(hours)+":"+String(minutes)+":0"+String(seconds)+":"+String(ticks)
-                }
-            }
-            
-            //change
+            //convert units
             if ticks == 100 {
                 seconds += 1
                 ticks = 0
@@ -62,16 +43,47 @@ class ViewController: UIViewController {
                 hours += 1
                 minutes = 0
             }
+            
+            //reset time-storing variable
+            time = ""
+            
+            //if there are any hours, append them to the time-storing variable first (so they appear on the left)
+            if hours > 0 {time.append(String(hours) + ":")}
+            
+            //if there are any minutes, append them to the time-storing variable next
+            if minutes > 0 {time.append(String(minutes) + ":")}
+            
+            //always append seconds to the time-storing variable next, even if just 00
+            if seconds < 10 {time.append("0" + String(seconds) + ":")} //if seconds only has one place, add another 0 for the tens place
+            else {time.append(String(seconds) + ":")} //otherwise just append seconds normally
+            
+            //always append ticks last (so that they appear on the right)
+            if ticks < 10 {time.append("0" + String(ticks))} //if ticks only has one place, add another 0 for the tens place
+            else {time.append(String(ticks))} //otherwise just append ticks normally
+            
+            
+            //update label
+            LabelOutlet.text = time
         }
     }
     
+    
+    
+    //establish time-storing variable
+    var time = ""
+    
+    //establish unit variables
     var ticks = 0
     var seconds = 0
     var minutes = 0
     var hours = 0
     
+    //establish buttonstate variable (stores if button is on/off)
     var buttonstate = 0
     
+//-----------------------------------------------------------------------------------------------------------
+    
+    //outlets
     @IBOutlet var LabelOutlet: UILabel!
     
     @IBOutlet var AppBG: UIImageView!
@@ -79,15 +91,21 @@ class ViewController: UIViewController {
     
     @IBOutlet var ButtonOutlet: UIButton!
     
+//-----------------------------------------------------------------------------------------------------------
+    
+    //when button is tapped
     @IBAction func ButtonAction(_ sender: Any) {
         
+        //switch what background is shown (all backgrounds switch to whatever they're not, hidden/shown)
         AppFace.isHidden = !AppFace.isHidden
         AppBG.isHidden = !AppBG.isHidden
         
+        //if buttonstate is 0 change button label to "Stop" and buttonstate to 1
         if buttonstate == 0 {
             ButtonOutlet.setTitle("Stop", for: .normal)
             buttonstate = 1
         }
+        //if buttonstate is 1 change button label to "Start" and buttonstate to 0
         else{
             ButtonOutlet.setTitle("Start", for: .normal)
             buttonstate = 0
